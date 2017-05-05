2a51661f46f094fb39f87f4fbe46a92d0732e8c6
Sarthak Manchanda <sarthakmanchanda1011@gmail.com>
2017-05-05 15:47:59 +0530

    first upload

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,99 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-appavailability.AppAvailability",
+        "file": "plugins/cordova-plugin-appavailability/www/AppAvailability.js",
+        "pluginId": "cordova-plugin-appavailability",
+        "clobbers": [
+            "appAvailability"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-actionsheet.ActionSheet",
+        "file": "plugins/cordova-plugin-actionsheet/www/ActionSheet.js",
+        "pluginId": "cordova-plugin-actionsheet",
+        "clobbers": [
+            "window.plugins.actionsheet"
+        ]
+    },
+    {
+        "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.Common",
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/common.js",
+        "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
+        "clobbers": [
+            "launchnavigator"
+        ]
+    },
+    {
+        "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.LaunchNavigator",
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/android/launchnavigator.js",
+        "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
+        "merges": [
+            "launchnavigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-appavailability": "0.4.2",
+    "cordova-plugin-console": "1.0.6",
+    "cordova-plugin-device": "1.1.5",
+    "cordova-plugin-compat": "1.1.0",
+    "cordova-plugin-geolocation": "2.4.3-dev",
+    "cordova-plugin-splashscreen": "4.0.2",
+    "cordova-plugin-statusbar": "2.2.2",
+    "cordova-plugin-whitelist": "1.3.2",
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-actionsheet": "2.3.3",
+    "uk.co.workingedge.phonegap.plugin.launchnavigator": "3.2.2"
+};
+// BOTTOM OF METADATA
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,144 @@
+angular.module('starter.controllers', [])
+
+
+
+.controller('DashCtrl', function($scope) {
+     $scope.logo = "<img class='logo-header' src='img/logo.png' height=36 width=90>";
+    
+     $scope.goWashroom = function(){
+        
+        //launching the app by navigation is simple usage 
+        launchnavigator.navigate("London, UK", {
+            //start: "Manchester, UK"
+            //when start is not specified then it will take the current location
+        }); 
+        
+        //launching the application but checking before first everything if app available 
+        launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, function(isAvailable){
+            var app;
+            if(isAvailable){
+                app = launchnavigator.APP.GOOGLE_MAPS;
+            }else{
+                console.warn("Google Maps not available - falling back to user selection");
+                app = launchnavigator.APP.USER_SELECT;
+            }
+            launchnavigator.navigate("London, UK", {
+                app: app
+            });
+        });
+        
+        //checking the platform of the device 
+        var platform = device.platform.toLowerCase();
+        if(platform == "android"){
+            platform = launchnavigator.PLATFORM.ANDROID;
+        }else if(platform == "ios"){
+            platform = launchnavigator.PLATFORM.IOS;
+        }
+         
+         //List all of the apps supported by the current platform
+         launchnavigator.getAppsForPlatform(platform).forEach(function(app){
+            console.log(launchnavigator.getAppDisplayName(app) + " is supported");
+        });
+         
+         //List apps available on the current device
+         launchnavigator.availableApps(function(results){
+            for(var app in results){
+                console.log(launchnavigator.getAppDisplayName(app) + (results[app] ? " is" : " isn't") +" available");
+            }
+        });
+    };
+         
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+   $scope.logo = "<img class='logo-header' src='img/logo.png' height=36 width=90>";
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+  $scope.logo = "<img class='logo-header' src='img/logo.png' height=36 width=90>";
+})
+
+.controller('AccountCtrl', function($scope,$ionicLoading) {
+  $scope.settings = {
+    enableFriends: true
+  };
+    
+    $scope.logo = "<img class='logo-header' src='img/logo.png' height=36 width=90>";
+    
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+        
+        
+ 
+        $scope.map = map;
+    });
+    
+//    var mapboxgl = require([node_modules/mapbox-gl/dist/mapbox-gl.js]);
+//        mapboxgl.accessToken = 'pk.eyJ1Ijoic2FydGhhazEwMTEiLCJhIjoiY2oxeW1jM2p4MDAwNjMzdXJsc2pyaHVudCJ9.CKM5enj3oY-w4d-v3BoSrw';
+//        var map = new mapboxgl.Map({
+//        container: 'map',
+//        style: 'mapbox://styles/mapbox/dark-v9'
+//    });   
+})
+
+.controller('driverCtrl', function($scope) {
+    $scope.logo = "<img class='logo-header' src='img/logo.png' height=36 width=90>";
+    
+})
+
+.controller("mapController",function($scope){
+    
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+ 
+        $scope.map = map;
+    });
+    
+});
